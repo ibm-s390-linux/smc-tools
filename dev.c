5c8f48a10a188efd838064db5f8729fd4b951a13
Guvenc Gulce <guvenc@linux.ibm.com>
2020-11-17 15:27:47 +0100

    smcr: Add generic netlink support for SMC-R devices
    Add generic netlink support for SMC-R devices.
    
    Signed-off-by: Guvenc Gulce <guvenc@linux.ibm.com>

--- dev.c
+++ dev.c
@@ -52,12 +52,18 @@ static struct nla_policy smc_gen_dev_smcd_sock_policy[SMC_NLA_DEV_MAX + 1] = {
 	[SMC_NLA_DEV_PCI_DEVICE]	= { .type = NLA_U16 },
 	[SMC_NLA_DEV_PCI_ID]		= { .type = NLA_NUL_STRING },
 	[SMC_NLA_DEV_PORT]		= { .type = NLA_NESTED },
+	[SMC_NLA_DEV_PORT2]		= { .type = NLA_NESTED },
+	[SMC_NLA_DEV_IB_NAME]		= { .type = NLA_NUL_STRING },
 };
 
 static struct nla_policy smc_gen_dev_port_smcd_sock_policy[SMC_NLA_DEV_PORT_MAX + 1] = {
 	[SMC_NLA_DEV_PORT_UNSPEC]	= { .type = NLA_UNSPEC },
 	[SMC_NLA_DEV_PORT_PNET_USR]	= { .type = NLA_U8 },
 	[SMC_NLA_DEV_PORT_PNETID]	= { .type = NLA_NUL_STRING },
+	[SMC_NLA_DEV_PORT_NETDEV]	= { .type = NLA_U32 },
+	[SMC_NLA_DEV_PORT_STATE]	= { .type = NLA_U8 },
+	[SMC_NLA_DEV_PORT_VALID]	= { .type = NLA_U8 },
+	[SMC_NLA_DEV_PORT_LNK_CNT]	= { .type = NLA_U32 },
 };
 
 static void usage(void)
@@ -184,17 +190,88 @@ static void show_devs_smcr_details(struct smc_diag_dev_info *dev, int idx)
 	}
 }
 
-static void show_dev_smcr_info(struct rtattr *tb[])
+static int fill_dev_port_smcr_struct(struct smc_diag_dev_info *dev, struct nlattr **attrs, int idx)
+{
+	struct nlattr *port_attrs[SMC_NLA_DEV_PORT_MAX + 1];
+
+	if (!attrs[SMC_NLA_DEV_PORT + idx]) {
+		dev->port_valid[idx] = 0;
+		return NL_OK;
+	}
+
+	if (nla_parse_nested(port_attrs, SMC_NLA_DEV_PORT_MAX,
+			     attrs[SMC_NLA_DEV_PORT + idx],
+			     smc_gen_dev_port_smcd_sock_policy)) {
+		fprintf(stderr, "failed to parse nested attributes: smc_gen_dev_port_smcd_sock_policy\n");
+		return NL_STOP;
+	}
+	if (port_attrs[SMC_NLA_DEV_PORT_PNETID])
+		snprintf((char*)&dev->pnet_id[idx], sizeof(dev->pnet_id[idx]), "%s",
+			 nla_get_string(port_attrs[SMC_NLA_DEV_PORT_PNETID]));
+	if (port_attrs[SMC_NLA_DEV_PORT_PNET_USR])
+		dev->pnetid_by_user[idx] = nla_get_u8(port_attrs[SMC_NLA_DEV_PORT_PNET_USR]);
+	if (port_attrs[SMC_NLA_DEV_PORT_NETDEV] && nla_get_u32(port_attrs[SMC_NLA_DEV_PORT_NETDEV]))
+			if_indextoname(nla_get_u32(port_attrs[SMC_NLA_DEV_PORT_NETDEV]), (char*)dev->netdev[idx]);
+	if (port_attrs[SMC_NLA_DEV_PORT_STATE])
+		dev->port_state[idx] = nla_get_u8(port_attrs[SMC_NLA_DEV_PORT_STATE]);
+	if (port_attrs[SMC_NLA_DEV_PORT_VALID])
+		dev->port_valid[idx] = nla_get_u8(port_attrs[SMC_NLA_DEV_PORT_VALID]);
+	if (port_attrs[SMC_NLA_DEV_PORT_LNK_CNT])
+		dev->lnk_cnt_by_port[idx] = nla_get_u32(port_attrs[SMC_NLA_DEV_PORT_LNK_CNT]);
+
+	return NL_OK;
+}
+
+static int fill_dev_smcr_struct(struct smc_diag_dev_info *dev, struct nlattr **attrs)
+{
+	struct nlattr *dev_attrs[SMC_NLA_DEV_MAX + 1];
+	int i;
+
+	if (nla_parse_nested(dev_attrs, SMC_NLA_DEV_MAX,
+			     attrs[SMC_GEN_DEV_SMCR],
+			     smc_gen_dev_smcd_sock_policy)) {
+		fprintf(stderr, "failed to parse nested attributes: smc_gen_dev_smcd_sock_policy\n");
+		return NL_STOP;
+	}
+	if (dev_attrs[SMC_NLA_DEV_IS_CRIT])
+		dev->is_critical = nla_get_u8(dev_attrs[SMC_NLA_DEV_IS_CRIT]);
+	if (dev_attrs[SMC_NLA_DEV_PCI_FID])
+		  dev->pci_fid = nla_get_u32(dev_attrs[SMC_NLA_DEV_PCI_FID]);
+	if (dev_attrs[SMC_NLA_DEV_PCI_CHID])
+		dev->pci_pchid = nla_get_u16(dev_attrs[SMC_NLA_DEV_PCI_CHID]);
+	if (dev_attrs[SMC_NLA_DEV_PCI_VENDOR])
+		dev->pci_vendor = nla_get_u16(dev_attrs[SMC_NLA_DEV_PCI_VENDOR]);
+	if (dev_attrs[SMC_NLA_DEV_PCI_DEVICE])
+		dev->pci_device = nla_get_u16(dev_attrs[SMC_NLA_DEV_PCI_DEVICE]);
+	if (dev_attrs[SMC_NLA_DEV_PCI_ID])
+		snprintf((char*)dev->pci_id, sizeof(dev->pci_id), "%s",
+			 nla_get_string(dev_attrs[SMC_NLA_DEV_PCI_ID]));
+	if (dev_attrs[SMC_NLA_DEV_IB_NAME])
+		snprintf((char*)dev->dev_name, sizeof(dev->dev_name), "%s",
+			 nla_get_string(dev_attrs[SMC_NLA_DEV_IB_NAME]));
+
+	for (i = 0; i < SMC_MAX_PORTS; i++) {
+		if (fill_dev_port_smcr_struct(dev, &dev_attrs[0], i) != NL_OK)
+			return NL_STOP;
+	}
+
+	return NL_OK;
+}
+
+static int show_dev_smcr_info(struct nlattr **attr)
 {
 	struct smc_diag_dev_info dev;
 	int i;
 
-	if (tb[SMC_DIAG_DEV_INFO_SMCR]) {
-		dev = *(struct smc_diag_dev_info *)RTA_DATA(tb[SMC_DIAG_DEV_INFO_SMCR]);
+	if (attr[SMC_GEN_DEV_SMCR]) {
+		if (fill_dev_smcr_struct(&dev, attr) != NL_OK)
+			return NL_STOP;
+
 		for (i = 0; i < SMC_MAX_PORTS; i++) {
 			show_devs_smcr_details(&dev, i);
 		}
 	}
+	return NL_OK;
 }
 
 static int fill_dev_port_smcd_struct(struct smc_diag_dev_info *dev, struct nlattr **attrs, int idx)
@@ -295,12 +372,13 @@ static int handle_gen_dev_reply(struct nl_msg *msg, void *arg)
 		return NL_STOP;
 	}
 
-	if (!attrs[SMC_GEN_DEV_SMCD])
+	if (!attrs[SMC_GEN_DEV_SMCD] && !attrs[SMC_GEN_DEV_SMCR])
 		return NL_STOP;
 
 	if (dev_smcd && attrs[SMC_GEN_DEV_SMCD])
 		rc = show_dev_smcd_info(&attrs[0]);
-
+	if (dev_smcr && attrs[SMC_GEN_DEV_SMCR])
+		rc = show_dev_smcr_info(&attrs[0]);
 	return rc;
 }
 
@@ -363,6 +441,8 @@ int invoke_devs(int argc, char **argv, int detail_level)
 	handle_cmd_params(argc, argv);
 	if (dev_smcd)
 		gen_nl_handle(SMC_NETLINK_GET_DEV_SMCD, handle_gen_dev_reply);
+	else
+		gen_nl_handle(SMC_NETLINK_GET_DEV_SMCR, handle_gen_dev_reply);
 
 	return 0;
 }
